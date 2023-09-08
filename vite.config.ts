ca52d562b1c078f0123ef013edd700dfbdc3f79b
JuanCManchegoH <juan.manchegoh@mainhightech.com>
2023-09-07 23:50:24 -0500

    [FIX] reload goes 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,8 @@
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
-
-const rewriteSlashToIndexHtml = () => {
-	return {
-		name: "rewrite-slash-to-index-html",
-		apply: "serve",
-		enforce: "post",
-		configureServer(server) {
-			// rewrite / as index.html
-			server.middlewares.use("/", (req, _, next) => {
-				if (req.url === "/") {
-					req.url = "/index.html";
-				}
-				next();
-			});
-		},
-	};
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	appType: "mpa",
-	plugins: [pluginRewriteAll(), rewriteSlashToIndexHtml()],
+	plugins: [react()],
 });
