a011d964d52660699b81adf12684f85d1885b337
JuanCManchegoH <juan.manchegoh@mainhightech.com>
2023-09-07 23:27:08 -0500

    [FIX] relod goes 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-const pluginRewriteAll = require("vite-plugin-rewrite-all");
+import { pluginRewriteAll } from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
