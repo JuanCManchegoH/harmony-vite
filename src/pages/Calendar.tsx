8daa74d38877ef12a0bed51d7558eac0d0c27f76
JuanCManchegoH <juan.manchegoh@mainhightech.com>
2023-10-13 08:46:25 -0500

    [FIX] unused pages disabled

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,25 +1,25 @@
-import { useEffect } from "react";
-import Background from "../common/Background";
-import Calendar from "../components/Calendar";
-import Navbar from "../components/Navbar";
-import { useAuth } from "../hooks/useAuth";
+// import { useEffect } from "react";
+// import Background from "../common/Background";
+// import Calendar from "../components/Calendar";
+// import Navbar from "../components/Navbar";
+// import { useAuth } from "../hooks/useAuth";
 
-export default function CalendarPage() {
-	const { getProfile } = useAuth();
+// export default function CalendarPage() {
+// 	const { getProfile } = useAuth();
 
-	useEffect(() => {
-		getProfile();
-	}, []);
+// 	useEffect(() => {
+// 		getProfile();
+// 	}, []);
 
-	return (
-		<>
-			<section className="h-full font-rhd">
-				<Navbar />
-				<main className="h-full pt-12">
-					<Calendar />
-				</main>
-				<Background />
-			</section>
-		</>
-	);
-}
+// 	return (
+// 		<>
+// 			<section className="h-full font-rhd">
+// 				<Navbar />
+// 				<main className="h-full pt-12">
+// 					<Calendar />
+// 				</main>
+// 				<Background />
+// 			</section>
+// 		</>
+// 	);
+// }
--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,25 +1,25 @@
-import { useEffect } from "react";
-import Background from "../common/Background";
-import Navbar from "../components/Navbar";
-import Statistics from "../components/Statistics";
-import { useAuth } from "../hooks/useAuth";
+// import { useEffect } from "react";
+// import Background from "../common/Background";
+// import Navbar from "../components/Navbar";
+// import Statistics from "../components/Statistics";
+// import { useAuth } from "../hooks/useAuth";
 
-export default function StatisticsPage() {
-	const { getProfile } = useAuth();
+// export default function StatisticsPage() {
+// 	const { getProfile } = useAuth();
 
-	useEffect(() => {
-		getProfile();
-	}, []);
+// 	useEffect(() => {
+// 		getProfile();
+// 	}, []);
 
-	return (
-		<>
-			<section className="h-full font-rhd">
-				<Navbar />
-				<main className="h-full pt-12">
-					<Statistics />
-				</main>
-				<Background />
-			</section>
-		</>
-	);
-}
+// 	return (
+// 		<>
+// 			<section className="h-full font-rhd">
+// 				<Navbar />
+// 				<main className="h-full pt-12">
+// 					<Statistics />
+// 				</main>
+// 				<Background />
+// 			</section>
+// 		</>
+// 	);
+// }
